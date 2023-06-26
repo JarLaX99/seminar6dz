47ea932cbe4c3e4317c02746ea67e733baf7541f
JarLaX99 <vanek10699@gmail.com>
2023-06-27 01:56:08 +0300

    семинар 6, задача 2

---
+++ task2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+//  заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void main()
+{
+    double b1 = ReadDouble("Введите b1: ");
+    double k1 = ReadDouble("Введите k1: ");
+    double b2 = ReadDouble("Введите b2: ");
+    double k2 = ReadDouble("Введите k2: ");
+    double x = IntersectionPoinX(b1, k1, b2, k2);
+    double y = IntersectionPoinY(b1, k1, b2, k2);
+    System.Console.WriteLine($"({x};{y})");
+}
+
+double ReadDouble(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double IntersectionPoinX(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    return x;
+}
+double IntersectionPoinY(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    return y;
+}
+main();
